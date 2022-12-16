8ef5ab8d01bf2fff3ebdf27a271478ef5fd60e76
c19719949 <57361393+c19719949@users.noreply.github.com>
2022-12-16 16:15:17 +0000

    Update index.js

---
+++ Lab4/Problem1/problem1/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+class note extends React.Component {
+    render(){
+        return(
+            <p>
+            test
+            </p>
+        );
+    }
+}
